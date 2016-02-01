28fe2cf0f314dcac147b64a91c4f8f764d41e287
Adam Johnston <ajohnston@alertsolutions.com>
2016-02-01 10:59:41 -0500

    v0.10.2 patch release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='freepy',
-    version='0.10.2rc2',
+    version='0.10.2',
     packages=find_packages(),
     dependency_links=[
         'https://github.com/alertsolutions/pykka/tarball/master#egg=pykka-1.3.0',
