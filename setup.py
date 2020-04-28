d2f2a3141a254ce6a766d5ce7064d55d79706d13
Adam Johnston <ajohnston@alertsolutions.com>
2020-04-28 16:20:37 -0400

    v0.10.5

--- setup.py
+++ setup.py
@@ -2,16 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='freepy',
-    version='0.10.4',
+    version='0.10.5',
     packages=find_packages(),
     dependency_links=[
         'https://github.com/alertsolutions/pykka/tarball/master#egg=pykka-1.3.0',
     ],
-    install_requires=[
-        'pykka==1.3.0',
-        'llist==0.4',
-        'twisted>=13.2.0'
-    ],
+    install_requires=['pykka', 'llist', 'twisted'],
     author='Thomas Quintana',
     author_email='quintana.thomas@gmail.com',
     license='Apache License 2.0',
